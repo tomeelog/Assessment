5849d562df78c1fed87a3498aeb703195618d7f4
tomeelog <tomeelog@gmail.com>
2021-07-03 22:43:46 +0100

    Authentication, event endpoints created

--- routes/events.js
+++ routes/events.js
@@ -4,14 +4,39 @@ const jsonfile = require('jsonfile');
 
 router.get('/getByDateRange', verify, (req, res) => {
 
-    // read json file
     const file = '../data.1625230922.json';
-    const ff = jsonfile.readFileSync(file);
+    const startDate = req.query.startDate;
+    const endDate = req.query.endDate;
+    const limit = req.query.limit;
+    const page = req.query.page;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    console.log("ReponseData", {li: limit, pa: page})
+    // read json file
+
+    const JsonData = jsonfile.readFileSync(file);
     const start_time = "2021-01-02T00:22:38.190Z";
     const end_time = "2021-01-02T00:34:59.819Z";
-    console.log("JsonData",ff.alarms.filter(item=> new Date(item.timestamp).getTime() > new Date(start_time).getTime() && new Date(item.timestamp) < new Date(end_time).getTime()));
 
-    
+    var result = JsonData.alarms.filter(item => new Date(item.timestamp).getTime() > new Date(start_time).getTime() && new Date(item.timestamp) < new Date(end_time).getTime())
+    const response = result.slice(startIndex, endIndex);
+    console.log("ReponseData", response);
+    res.json(response);
+    // console.log("JsonData",JsonData.alarms.filter(item=> new Date(item.timestamp).getTime() > new Date(start_time).getTime() && new Date(item.timestamp) < new Date(end_time).getTime()));
+
 });
+
+router.get('/GetLocationById', (req, res) => {
+    const file = '../data.1625230922.json';
+    const locationId = req.query.id;
+    console.log("locationId", locationId);
+    const JsonData = jsonfile.readFileSync(file);
+    var result = JsonData.locations.filter(item => item.id == locationId);
+
+    console.log("ReponseData", result);
+
+    res.json(result);
+});
+
 
 module.exports = router; 
